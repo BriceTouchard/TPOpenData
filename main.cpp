574d8e3719c1b808e698b8562e4b78ac47fa7a73
Brice Touchard <touchardbrice@gmail.com>
2023-09-20 16:08:00 +0200

    maj 0.3.1

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int spacing = 10;
 
 int i = 0;
 int h, hMax, hMin, ecart;
-float coeff;
+float coeff, scaleMin, scaleMax;
 string grandeur_hydro;
 
 string request(string url){
@@ -60,11 +60,21 @@ string request(string url){
     return respStr;
 }
 
+/**
+ * @brief y
+ * @param val
+ * @return l'ordonnée du haut des barres de l'histogramme
+ */
 int y(int val){
     int y = imgsizeY-marginY-(val-hMin+ecart/5)*coeff;
     return y;
 }
 
+/**
+ * @brief floatToUcEtoile
+ * @param f
+ * @return le casting d'un float en unsigned char*
+ */
 unsigned char* floatToUcEtoile(float f){
     int i = static_cast<int>(f);
     string str = to_string(i);
@@ -74,15 +84,21 @@ unsigned char* floatToUcEtoile(float f){
     return ucEtoile;
 }
 
-void dessinHistogramme(json resultsObs, gdImagePtr im, int* colors){
-    for (json &result : boost::adaptors::reverse(resultsObs)){   // la requête sort dans l'ordre anti-chronologique, donc on inverse
+/**
+ * @brief dessinHistogramme
+ * @param jDataObs
+ * @param im1
+ * @param colors
+ */
+void dessinHistogramme(json jDataObs, gdImagePtr im1, int* colors){
+    for (json &result : boost::adaptors::reverse(jDataObs)){   // la requête sort dans l'ordre anti-chronologique, donc on inverse
         h = static_cast<int>(result["resultat_obs"]);
         if(grandeur_hydro == "Q") h=h/100; // conversion en m³/s
         cout << "Hauteur = " << result["resultat_obs"] << " | Date et heure  = " << result["date_obs"].get<string>() << endl;
 
         // Dessin des barres de l'histogramme
         int x = marginX+(barWidth+spacing)*i;
-        gdImageFilledRectangle(im,x,imgsizeY-marginY,x+barWidth,y(h),colors[2]);
+        gdImageFilledRectangle(im1,x,imgsizeY-marginY,x+barWidth,y(h),colors[2]);
 
         // Formattage de la grandeur (H ou Q) et des heures
         string dateStr = result["date_obs"].get<string>();
@@ -98,93 +114,20 @@ void dessinHistogramme(json resultsObs, gdImagePtr im, int* colors){
         }
         // Affichage des hauteurs (en mm) puis des heures (hh:mm)
         int lengthH = resObsStr.length();
-        gdImageString(im,gdFontGetGiant (),x+(27-lengthH*5),y(h)-16,(unsigned char*) resObsStr.c_str(),colors[1]);
-        gdImageString(im,gdFontGetGiant (),x+5,imgsizeY-marginY+5,(unsigned char*) heure.c_str(),colors[1]);
+        gdImageString(im1,gdFontGetGiant (),x+(27-lengthH*5),y(h)-16,(unsigned char*) resObsStr.c_str(),colors[1]);
+        gdImageString(im1,gdFontGetGiant (),x+5,imgsizeY-marginY+5,(unsigned char*) heure.c_str(),colors[1]);
         i++;
     }
 }
 
-int main(int argc, char *argv[])
-{
-    // Message à l'utilisateur s'il a oublié un ou plusieurs arguments
-    if(argc < 3) cout << "Veuillez saisir tous les arguments requis : grandeur souhaitée (H ou Q) et le code station. Exemple : H H208000104" << endl;
+/**
+ * @brief makeScale. On détermine les valeurs mini et maxi, hMin, hMax, l'écart et le coeff. Valeurs utiles à la fonction y().
+ * @param jDataObs
+ */
+void makeScale(json jDataObs){
 
-    grandeur_hydro = argv[1];
-    string station = argv[2];
-
-    // Requêtes HTTP GET
-    // 10 dernières valeurs de H ou Q pour une station donné
-    string respStrObs = request(urlBase + "observations_tr?code_entite=" + station + "&size=10&grandeur_hydro=" + grandeur_hydro + "&fields=date_obs,resultat_obs");
-    cout << endl;
-    // Libellé d'un site pour une station donnée
-    string respStrStation = request(urlBase + "referentiel/stations?code_station=" + station + "&fields=libelle_site");
-
-    // Liste des code stations d'une région
-    string code_region = "32"; // Hauts-de-France
-    string reqCodesByRegion = request(urlBase + "referentiel/stations?fields=code_station&code_region=" + code_region);
-
-    cout << endl;
-    cout << "reqCodesByRegion = " << reqCodesByRegion << endl;
-
-    // Libgd
-    gdImagePtr im;
-    FILE *pngout;
-
-    im = gdImageCreate(imgsizeX, imgsizeY);
-
-    int bckgrd, noir, bleu, vert;
-    bckgrd = gdImageColorAllocate(im, 240, 240, 255);
-    noir =   gdImageColorAllocate(im, 0, 0, 0);
-    bleu =    gdImageColorAllocate(im, 50, 100, 255);
-    vert =    gdImageColorAllocate(im, 50, 255, 50);
-    int colors[4] = {bckgrd, noir, bleu, vert};
-
-    gdFontPtr fonts[5];
-    fonts[0] = gdFontGetTiny ();
-    fonts[1] = gdFontGetSmall ();
-    fonts[2] = gdFontGetMediumBold ();
-    fonts[3] = gdFontGetLarge ();
-    fonts[4] = gdFontGetGiant ();
-
-    // Extraction des données (data)
-    json jObs = json::parse(respStrObs);
-    json jStations = json::parse(respStrStation);
-    json jCodesByRegion = json::parse(reqCodesByRegion);
-
-    json &resultsObs = jObs["data"];
-    json &resultsStations = jStations["data"];
-    json &resCodesByRegion = jCodesByRegion["data"];
-
-    // Get de la dernière valeur de H pour chaque station des hauts-de-france
-    cout << endl;
-    cout << "code station = " << endl;
-    for (json &res : resCodesByRegion) {
-//        cout << res["code_station"].get<string>() << ", ";
-//        string reqVal = request(urlBase + "observations_tr?code_entite=" + res["code_station"].get<string>() + "&size=1&grandeur_hydro=H&fields=resultat_obs");
-
-    }
-    cout << endl;
-
-    if(resultsObs.empty()){
-        cout << endl;
-        cout << "Pas de données pour la grandeur hydrométrique sélectionnée. Veuillez choisir une autre station ou une autre grandeur." <<  endl;
-    }
-    // Extraction de la date d'observation
-    json jObs0 = resultsObs[0];
-    string date_obs = jObs0["date_obs"];
-
-    // Extraction du libellé du site
-    json jStation0 = resultsStations[0];
-    string libelle_site = jStation0["libelle_site"];
-
-    // Création du nom de fichier (fileName). Exemple : H_H208000104_2023-09-19T08:10:00Z.png
-    string fileNameStr = grandeur_hydro + "_"  + station + "_" + date_obs + ".png";
-    const char* fileName = fileNameStr.c_str();
-
-    // On détermine les hauteurs minimale et maximale, hMin et hMax, ainsi que l'écart entre les deux
     // la requête sort dans l'ordre chronologique descendant, donc on inverse avec l'adaptateur reverse de la librairie Boost
-
-    for (json &result : boost::adaptors::reverse(resultsObs)){
+    for (json &result : boost::adaptors::reverse(jDataObs)){
         h = static_cast<int>(result["resultat_obs"]);
         if(grandeur_hydro == "Q") h=h/100; // conversion en m³/s
         if(h > hMax){
@@ -194,20 +137,19 @@ int main(int argc, char *argv[])
             hMin = h;
         }else if(h <  hMin){
             hMin = h;
-        }        
+        }
     }
     cout << "hMin = " << hMin << endl;
     ecart = hMax - hMin;
     coeff = 300.0f/ecart; // l'écart entre valeur min et valeur max s'affiche sur 300 px.
 
     // On calcule les valeurs d'échelle min et max, respectivement scaleMin et scaleMax
-    float scaleMin, scaleMax;
     float hMinF = hMin;
     float hMaxF = hMax;
     hMinF = hMinF/100;
     hMaxF = hMaxF/100;
 
-    // ScaleMin
+    // ScaleMin, valeur d'échellle min
     if(round(hMinF) > floor(hMinF)){
         scaleMin = (floor(hMinF)+0.5)*100;
     } else if(round(hMinF) == floor(hMinF)){
@@ -220,7 +162,7 @@ int main(int argc, char *argv[])
     cout << "scaleMin = " << scaleMin << endl;
     cout << "y(scaleMin) = " << y(scaleMin) << " | imgSizeY = " << imgsizeY << endl;
 
-    // ScaleMax. Valeur arrrondie à 100, 50 ou 25. Si l'écart on affiche une valeur exacte.
+    // ScaleMax, valeur d'échellle min. Arrrondie à 100, 50 ou 25 ou une valeur exacte si on est hors cadre.
     cout << "hMax = " << hMax << endl;
 
     if(round(hMaxF) != floor(hMinF)){
@@ -237,16 +179,121 @@ int main(int argc, char *argv[])
     if(y(scaleMax) < marginY){ // Si avec la valeur maximale arrondie on est hors cadre, on n'arrondi pas
         scaleMax = hMax;
     }
-    // Dessin du graphe
-    dessinHistogramme(resultsObs, im, colors);
+}
+
+int main(int argc, char *argv[])
+{
+    // Message à l'utilisateur s'il a oublié un ou plusieurs arguments
+    if(argc < 3) cout << "Veuillez saisir tous les arguments requis : grandeur souhaitée (H ou Q) et le code station. Exemple : H H208000104" << endl;
+
+    grandeur_hydro = argv[1];
+    string code_station = argv[2];
+
+    // --- Requêtes HTTP GET ------
+    // 10 dernières valeurs de H ou Q pour une station donné
+    string reqObs = request(urlBase + "observations_tr?code_entite=" + code_station + "&size=10&grandeur_hydro=" + grandeur_hydro + "&fields=date_obs,resultat_obs");
+    cout << endl;
+
+    // Libellé d'un site pour un code station donné
+    string reqLibelleSiteByCode = request(urlBase + "referentiel/stations?code_station=" + code_station + "&fields=libelle_site");
+    cout << endl;
+
+    // resultat_obs et code_station dont le code_entite commence par un H à un instant t
+    string reqObsH = request(urlBase + "observations_tr?code_entite=H*&grandeur_hydro=H&fields=resultat_obs,code_station&date_debut_obs=2023-09-20T08:00:00Z&date_fin_obs=2023-09-20T08:00:00Z");
+    cout << endl;
+
+    // Extraction des données (data)
+    json jObs = json::parse(reqObs);
+    json jStations = json::parse(reqLibelleSiteByCode);    
+    json jObsH = json::parse(reqObsH);
+
+    json &jDataObs = jObs["data"];
+    json &jDataStations = jStations["data"];    
+    json &jDataObsH = jObsH["data"];
+
+    // --- Idée 3 mettre les valeurs dans un vector de pair --------
+    // Exemple "code_station": "H002001001",    "resultat_obs": 236.0
+    vector< pair<string,int> > vectH;
+
+    // Initialisation du vecteur vectH
+    for (json &j : jDataObsH){
+        const pair<string,int> p(j["code_station"].get<string>(),j["resultat_obs"]);
+        vectH.push_back(p);
+    }
+    // Classement dans l'ordre décroissant des secondes valeurs
+    // avec une expression lambda comme comparateur
+    sort(vectH.begin(), vectH.end(), [](auto &left, auto &right) {
+        return left.second > right.second;
+    });
+
+    // Affiche les 10 premières paires de valeurs
+    for (auto it = vectH.cbegin() ; it != vectH.cbegin()+10; ++it){
+        cout << it->first << " " << it->second << " ";
+    }
+    cout << endl;
+
+    if(jDataObs.empty()){
+        cout << endl;
+        cout << "Pas de données pour la grandeur hydrométrique sélectionnée. Veuillez choisir une autre station ou une autre grandeur." <<  endl;
+    }
+    // Extraction de la date d'observation et du libellé du site
+    string date_obs = jDataObs[0]["date_obs"];
+    string libelle_site = jDataStations[0]["libelle_site"];
+
+    // Création du nom de fichier (fileName). Exemple : H_H208000104_2023-09-19T08:10:00Z.png
+    string fileNameStr = grandeur_hydro + "_"  + code_station + "_" + date_obs + ".png";
+    const char* fileName = fileNameStr.c_str();
+
+    // Fonction créant l'échelle
+    makeScale(jDataObs);
+
+    // -- Libgd -----------
+    gdImagePtr im1;
+    gdImagePtr im2;
+    FILE *pngout1;
+    FILE *pngout2;
+
+    im1 = gdImageCreate(imgsizeX, imgsizeY);
+    im2 = gdImageCreate(imgsizeX, imgsizeY);
+
+    int bckgrd, noir, bleu, vert;
+    bckgrd = gdImageColorAllocate(im1, 240, 240, 255);
+    noir =   gdImageColorAllocate(im1, 0, 0, 0);
+    bleu =    gdImageColorAllocate(im1, 50, 100, 255);
+    vert =    gdImageColorAllocate(im1, 50, 255, 50);
+    bckgrd = gdImageColorAllocate(im2, 240, 240, 255);
+    noir =   gdImageColorAllocate(im2, 0, 0, 0);
+    bleu =    gdImageColorAllocate(im2, 50, 100, 255);
+    vert =    gdImageColorAllocate(im2, 50, 255, 50);
+    int colors[4] = {bckgrd, noir, bleu, vert};
+
+    gdFontPtr fonts[5];
+    fonts[0] = gdFontGetTiny ();
+    fonts[1] = gdFontGetSmall ();
+    fonts[2] = gdFontGetMediumBold ();
+    fonts[3] = gdFontGetLarge ();
+    fonts[4] = gdFontGetGiant ();
+
+    // Dessin du graphe 1
+    dessinHistogramme(jDataObs, im1, colors);
+
+    // Dessin du graphe 2
+    i=0;
+    for (auto it = vectH.cbegin() ; it != vectH.cbegin()+10; ++it){
+//        cout << it->first << " " << it->second << " ";
+        int x = marginX+(barWidth+spacing)*i;
+        cout <<  "y(it->second) = " << y(it->second) << endl;
+        gdImageFilledRectangle(im2,x,imgsizeY-marginY,x+barWidth,y(it->second),colors[2]);
+        i++;
+    }
 
     // Axe (abcisse et ordonnée)
-    gdImageLine(im,marginX,imgsizeY-marginY,imgsizeX-marginX,imgsizeY-marginY,noir); // abcisse
-    gdImageLine(im,marginX,imgsizeY-marginY*0.5,marginX,marginY,noir); // ordonnée
-    gdImageLine(im,imgsizeX-marginX,imgsizeY-marginY,imgsizeX-marginX-8,imgsizeY-marginY-6,noir); // trait flèche abcisse 1
-    gdImageLine(im,imgsizeX-marginX,imgsizeY-marginY,imgsizeX-marginX-8,imgsizeY-marginY+6,noir); // trait flèche abcisse 2
-    gdImageLine(im,marginX,marginY,marginX-6,marginY+8,noir); // trait flèche ordonnée 1
-    gdImageLine(im,marginX,marginY,marginX+6,marginY+8,noir); // trait flèche ordonnée 2
+    gdImageLine(im1,marginX,imgsizeY-marginY,imgsizeX-marginX,imgsizeY-marginY,noir); // abcisse
+    gdImageLine(im1,marginX,imgsizeY-marginY*0.5,marginX,marginY,noir); // ordonnée
+    gdImageLine(im1,imgsizeX-marginX,imgsizeY-marginY,imgsizeX-marginX-8,imgsizeY-marginY-6,noir); // trait flèche abcisse 1
+    gdImageLine(im1,imgsizeX-marginX,imgsizeY-marginY,imgsizeX-marginX-8,imgsizeY-marginY+6,noir); // trait flèche abcisse 2
+    gdImageLine(im1,marginX,marginY,marginX-6,marginY+8,noir); // trait flèche ordonnée 1
+    gdImageLine(im1,marginX,marginY,marginX+6,marginY+8,noir); // trait flèche ordonnée 2
 
     // Ligne d'échelle min et max
     int yMin =y(scaleMin);
@@ -254,22 +301,22 @@ int main(int argc, char *argv[])
     cout << "scaleMin = " << scaleMin << " | scaleMax = " << scaleMax << endl;
     cout << "yMin = " << yMin << " | yMax = " << yMax << endl;
 
-    gdImageLine(im,marginX-5,yMin,marginX+5,yMin,noir);
-    gdImageLine(im,marginX-5,yMax,marginX+5,yMax,noir);
+    gdImageLine(im1,marginX-5,yMin,marginX+5,yMin,noir);
+    gdImageLine(im1,marginX-5,yMax,marginX+5,yMax,noir);
 
     // Affichage des valeurs min et max de l'échelle
     unsigned char* scaleMinUc = floatToUcEtoile(scaleMin);
     int lengthScaleMin = strlen((char*) scaleMinUc);
-    gdImageString(im,fonts[4], marginX-lengthScaleMin*15, yMin-10 , scaleMinUc, noir);
+    gdImageString(im1,fonts[4], marginX-lengthScaleMin*15, yMin-10 , scaleMinUc, noir);
 
     unsigned char* scaleMaxUc = floatToUcEtoile(scaleMax);
     int lengthScaleMax = strlen((char*) scaleMaxUc);
-    gdImageString(im,fonts[4], marginX-lengthScaleMax*15, yMax-10 , scaleMaxUc, noir);
+    gdImageString(im1,fonts[4], marginX-lengthScaleMax*15, yMax-10 , scaleMaxUc, noir);
 
     // Affichage du titre
     unsigned char* titre = (unsigned char*) libelle_site.c_str();
     int lengthTitre = strlen((char*) titre);
-    gdImageString(im,fonts[4], imgsizeX/2-lengthTitre*5, marginY/2, titre , noir);
+    gdImageString(im1,fonts[4], imgsizeX/2-lengthTitre*5, marginY/2, titre , noir);
 
     unsigned char* grandeurDesc;
     if(grandeur_hydro == "H"){
@@ -278,17 +325,21 @@ int main(int argc, char *argv[])
         grandeurDesc = (unsigned char*) "Debit Q en m3/s";
     }
     int lengthGrandeurDesc = strlen((char*) grandeurDesc);
-    gdImageString(im,fonts[4], imgsizeX/2-lengthGrandeurDesc*5, marginY/3, grandeurDesc , noir);
+    gdImageString(im1,fonts[4], imgsizeX/2-lengthGrandeurDesc*5, marginY/3, grandeurDesc , noir);
 
     // Affichage de la date
     string date_jour = date_obs.substr (0,10);
 
-    gdImageString(im,fonts[4], imgsizeX/2-date_jour.length()*6, imgsizeY-marginY/2, (unsigned char*) date_jour.c_str(), noir);
+    gdImageString(im1,fonts[4], imgsizeX/2-date_jour.length()*6, imgsizeY-marginY/2, (unsigned char*) date_jour.c_str(), noir);
 
-    pngout = fopen(fileName, "wb");
-    gdImagePng(im, pngout);
-    fclose(pngout);
-    gdImageDestroy(im);
+    pngout1 = fopen(fileName, "wb");
+    pngout2 = fopen("fileNameProvisoire", "wb");
+    gdImagePng(im1, pngout1);
+    gdImagePng(im2, pngout2);
+    fclose(pngout1);
+    fclose(pngout2);
+    gdImageDestroy(im1);
+    gdImageDestroy(im2);
 
     return 0;
 }
